eb9bfcc284a21489da466a584ce8c1f14b05f053
Thato <thatophohlela.com>
2021-05-31 03:14:01 +0200

    p

---
+++ greetingsJS/greetEnglish.js
@@ -0,0 +1,121 @@
+var nameString = document.querySelector(".enterName");
+var helloPlusName = document.querySelector(".greetingAndName");
+var buttonForGreetMe = document.querySelector(".greetMeBtn");
+var counterRef = document.querySelector(".counter1")
+var emptyStringRef = document.querySelector(".greetingAndName1");
+var resetBtnRef = document.querySelector(".resetBtn")
+
+
+
+var count = 0;
+
+var dataFromLocal
+if (localStorage['names']) {
+    dataFromLocal = JSON.parse(localStorage.getItem('names'));
+} else {
+    dataFromLocal = {}
+}
+var namesGreeted = dataFromLocal;
+
+
+var retrievedCount;
+var greetingsInstance = greetings(dataFromLocal);
+var retrievedNames;
+
+
+myVar;
+counterRef.innerHTML = greetingsInstance.getCount();
+
+
+
+function clearTextArea() {
+
+    nameString.value = null;
+
+}
+
+function clearEmptyStringArea() {
+    emptyStringRef.innerHTML = " ";
+}
+
+function clearGreetingArea() {
+    helloPlusName.innerHTML = " ";
+}
+
+function clearCounterArea() {
+    counterRef.innerHTML = " ";
+}
+
+ var myVar;
+
+function myFunction() {
+    myVar = setTimeout(function () {
+        emptyStringRef.innerHTML = null;
+    }
+        , 3000);
+}
+
+
+function greet() {
+    var name = nameString.value;
+    myFunction();
+    
+    var radioBtnEng = document.querySelector("input[name='langRadioBtn']:checked");
+
+
+
+    nameString.value = null;    
+  
+   
+
+
+   
+    if (!radioBtnEng){
+        
+        emptyStringRef.classList.add('danger');
+        emptyStringRef.innerHTML = greetingsInstance.errors(name,langValue);
+        clearGreetingArea();
+       
+    }
+
+
+    if (radioBtnEng) {
+       
+        var langValue = radioBtnEng.value;
+
+        radioBtnEng.checked = false;
+        emptyStringRef.classList.add('danger');  
+     emptyStringRef.innerHTML = greetingsInstance.errors(name,langValue);
+     
+
+       
+        helloPlusName.innerHTML = greetingsInstance.greetings1(name,langValue)
+        localStorage['names'] = JSON.stringify(namesGreeted);
+        var list = Object.keys(dataFromLocal).length;
+        counterRef.innerHTML = list;
+        
+    }
+}
+
+
+
+
+
+function reset() {
+    count1 = 0;
+    counterRef.innerHTML = 0;
+
+
+    localStorage.clear();
+    localStorage['spot'] = 0;
+    location.reload();
+
+
+
+    clearGreetingArea();
+    clearEmptyStringArea();
+}
+
+
+buttonForGreetMe.addEventListener('click', greet);
+resetBtnRef.addEventListener('click', reset);
---
+++ greetingsUnitTesting/greetingsFactoryFunction.js
@@ -0,0 +1,85 @@
+function greetings(names) {
+
+    var namesGreeted = names || {};
+   var namesGreeted2 = {};
+
+
+function errors(name,langValue){
+    if (!name && !langValue) {
+        return 'Please enter name and choose language'
+    }
+    else if (!langValue ) {
+        return 'Please choose language'
+    }
+
+    else if (name === "" || name == Number(name)) {
+        return 'Please enter name'
+
+    }
+
+    else return null;
+}
+
+
+    function greetings1(name,langValue) {
+        var name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+
+        addNames(name,langValue)
+        errors(name,langValue)
+        
+
+         if (name === "" || name == Number(name)) {
+            return null;
+    
+        }
+
+        else if (langValue === "English") {
+            return 'Hello, ' + name;
+
+        } else if (langValue === "Afrikaans") {
+            return 'Hallo, ' + name;
+
+        } else if (langValue === "isiXhosa") {
+            return 'Molo, ' + name;
+        }
+
+
+
+    }
+
+
+
+    function addNames(name,langValue) {
+        if (name === "" || name == Number(name)){
+            namesGreeted2[name];
+            return null;
+        }
+        else if (langValue && namesGreeted[name] === undefined) {
+            namesGreeted[name] = 0;
+            return null;
+        }
+    }
+
+
+
+    function getNames() {
+        return namesGreeted;
+    }
+
+
+    function getCount() {
+        return Object.keys(namesGreeted).length;
+    }
+
+
+
+
+    return {
+        addNames,
+        greetings1,
+        errors,
+        getNames,
+        getCount,
+         
+    }
+}
